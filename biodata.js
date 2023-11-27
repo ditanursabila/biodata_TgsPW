ae5994fb71d47dde9be9366d6c76c4ce00bdf1af
Ditaaa <113565619+ditanursabila@users.noreply.github.com>
2023-11-27 17:53:24 +0700

    Add files via upload

---
+++ biodata.js
@@ -0,0 +1,23 @@
+const NIS = 222310222;
+const NO_ABSEN = 7;
+let nama = "Dita nur sabila";
+let kelas = "XI PPLG 2";
+let jurusan = "PPLG";
+let hobi = "Memasak";
+let favFood = "Sate";
+let favDrink = "Matcha latte";
+let citaCita = "Front-end developer";
+let alamat = "Kp.Pangadegan, Rt.03 Rw.01, kec.Cibeureum, kel.KotaBaru, Kota Tasikmalaya, Jawa barat";
+
+document.write(
+  "NIS: " + NIS,
+  "<br>Nomor Absen: " + NO_ABSEN,
+  "<br>Nama: " + nama,
+  "<br>Kelas: " + kelas,
+  "<br>Jurusan: " + jurusan,
+  "<br>Hobi: " + hobi,
+  "<br>Makanan Favorit: " + favFood,
+  "<br>Minuman Favorit: " + favDrink,
+  "<br>Cita-Cita: " + citaCita,
+  "<br>Alamat: " + alamat
+);
